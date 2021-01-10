6b8044dfde22c6bc366776ff76d6d27434764643
Marcos L. Souza <marcoslsouzadf@gmail.com>
2021-01-10 00:46:13 -0300

    Implementacao de tab para separar formulario de cadastro e tabela de listagem

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -11,19 +11,40 @@ import { Contato } from './contato';
 export class ContatoComponent implements OnInit {
 
   formulario : FormGroup;
+  contatos : Contato[] = [];
 
   constructor(private service : ContatoService, private fb : FormBuilder) { }
 
   ngOnInit(): void {
     this.formulario = this.fb.group({
       nome : ['', Validators.required],
-      email : ['', Validators.email]
+      email : ['', [Validators.email, Validators.required]]
     });
   }
 
   submit() {
-    console.log(this.formulario.value);
-    //this.service.save(c).subscribe(resposta => { console.log(resposta); })
+    //console.log(this.formulario.value);
+    //const isValid = this.formulario.valid;
+    //console.log('Valid', isValid);
+    
+   /* let erroNomeRequired;
+    if(this.formulario.controls.nome.errors) {
+      erroNomeRequired = this.formulario.controls.nome.errors.required;
+      console.log('erroNomeRequired : ', erroNomeRequired);
+    }
+    let erroEmailInvalio;
+    if( this.formulario.controls.email.errors) {
+      erroEmailInvalio =  this.formulario.controls.email.errors.email;
+      console.log('erroEmailInvalio : ', erroEmailInvalio);
+    }*/
+    
+    const formValues = this.formulario.value;
+    const contato : Contato = new Contato(formValues.nome, formValues.email);
+    this.service.save(contato).subscribe(resposta => { 
+      // console.log(resposta); 
+      this.contatos.push(resposta);
+      console.log(this.contatos);
+    })
   }
 
 }
